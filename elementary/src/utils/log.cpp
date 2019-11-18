4e6ecea42c962dc2338b225fa2a818f3a2823eaf
Sahil Iyer <iyersahil12@gmail.com>
2019-11-18 22:26:14 +1100

    Colours 'Error' in red on Windows.

--- elementary/src/utils/log.cpp
+++ elementary/src/utils/log.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
 #include <cstdarg>
 
+#if defined(ELEMENTARY_WIN32)
+#include <windows.h>
+#endif
+
 #include <SDL/SDL.h>
 
 #include "utils/log.h"
@@ -11,7 +15,25 @@ void error(const char* msg, ...)
 	std::va_list args;
 	va_start(args, msg);
 	
+#if defined(ELEMENTARY_WIN32)
+	// TODO(fkp): Extract into a function
+	// Gets colour info
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(hConsole, &info);
+	WORD normalColour = info.wAttributes;
+
+	// Colours "Error" in red
+	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+#endif
+
 	std::fprintf(stderr, "Error: ");
+	
+#if defined(ELEMENTARY_WIN32)
+	// Returns back to original colour
+	SetConsoleTextAttribute(hConsole, normalColour);
+#endif
+
 	std::vfprintf(stderr, msg, args);
 	std::fprintf(stderr, "\n");
 
@@ -25,7 +47,25 @@ void sdlError(const char* msg, ...)
 	std::va_list args;
 	va_start(args, msg);
 	
-	std::fprintf(stderr, "Error: ");
+	#if defined(ELEMENTARY_WIN32)
+		// TODO(fkp): Extract into a function
+		// Gets colour info
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_SCREEN_BUFFER_INFO info;
+		GetConsoleScreenBufferInfo(hConsole, &info);
+		WORD normalColour = info.wAttributes;
+
+		// Colours "Error" in red
+		SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+	#endif
+
+		std::fprintf(stderr, "Error: ");
+	
+	#if defined(ELEMENTARY_WIN32)
+		// Returns back to original colour
+		SetConsoleTextAttribute(hConsole, normalColour);
+	#endif
+
 	std::vfprintf(stderr, msg, args);
 	std::fprintf(stderr, "\nSDL_Error: %s\n", SDL_GetError());
 
