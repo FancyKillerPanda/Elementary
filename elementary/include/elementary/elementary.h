6eac7f9ed7e2e1ea38391b4a1ae8f46fd0b83d26
Sahil Iyer <iyersahil12@gmail.com>
2019-12-02 18:44:28 +1100

    Moved #include maths.h to top of utils includes.

--- elementary/include/elementary/elementary.h
+++ elementary/include/elementary/elementary.h
@@ -1,11 +1,11 @@
 #pragma once
 
+#include "utils/maths.h"
 #include "utils/log.h"
 #include "utils/init.h"
 #include "utils/console_colour.h"
 #include "utils/random.h"
 #include "utils/timer.h"
-#include "utils/maths.h"
 
 #include "gfx/window.h"
 #include "gfx/texture.h"
