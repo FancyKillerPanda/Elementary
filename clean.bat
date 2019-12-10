c6dcb65dd2c4c02a01413137a9a3fb7b5431c5af
Sahil Iyer <iyersahil12@gmail.com>
2019-12-11 00:44:05 +1100

    Made clean.bat work for both MSVC and clang++.

--- clean.bat
+++ clean.bat
@@ -1,14 +1,20 @@
 @echo off
 
-IF NOT EXIST build\ goto :eof
-pushd build
+call :Clean build
+call :Clean build-clang
+exit /B 0
+
+:Clean
+IF NOT EXIST %~1\ exit /B 1
+pushd %~1
 
 rem Deletes intermediate directories
-RMDIR /S /Q elementary-int >nul 2>nul
-RMDIR /S /Q sandbox-int >nul 2>nul
+rmdir /S /Q elementary-int >nul 2>nul
+rmdir /S /Q sandbox-int >nul 2>nul
 
 rem Deletes output files
 del elementary.lib >nul 2>nul
 del sandbox.exe >nul 2>nul
 
 popd
+exit /B 0
